fa089144e88b175c8b446c477ae6ba2f01637aa0
Murasaki Bara <viole@Nico-Robin.com>
2017-04-10 08:02:27 -0400

    Recent commit

--- src/user/details/validate.java
+++ src/user/details/validate.java
@@ -1,4 +1,5 @@
 package user.details;
+import java.util.Calendar;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.io.IOException;
@@ -116,5 +117,102 @@ public class validate {
 				}
 			}
 			return check;
+		}
+		public boolean validateDate(String date)
+		{
+			Calendar cal = Calendar.getInstance();
+			boolean check = false;		
+			
+			if(date.matches("\\d{4}-\\d{2}-\\d{2}"))
+			{
+				int y = Integer.parseInt(date.substring(0, 4));
+				int m = Integer.parseInt(date.substring(5, 7));
+				int d = Integer.parseInt(date.substring(8, date.length()));
+				
+				if(m > cal.get(Calendar.MONTH) && m <= 12 && y >= cal.get(Calendar.YEAR))
+				{
+					if(d >= cal.get(Calendar.DATE) && d <= dayInMonth(y, m))
+						check = true;
+				}
+			}
+			return check;
+		}
+		public boolean validateFromTime(String from, String date)
+        {
+            Calendar cal = Calendar.getInstance();
+            boolean check = false;    
+            if(from.matches("\\d{2}:\\d{2}"))
+            {
+                int h = Integer.parseInt(from.substring(0, 2));
+                int m = Integer.parseInt(from.substring(3, from.length()));
+                int y = Integer.parseInt(date.substring(0, 4));
+                int mon = Integer.parseInt(date.substring(5, 7));
+                int d = Integer.parseInt(date.substring(8, date.length()));
+                
+                if(d == cal.get(Calendar.DATE) && y == cal.get(Calendar.YEAR) && mon == (cal.get(Calendar.MONTH) + 1) && h >= cal.get(Calendar.HOUR_OF_DAY) && h <= 23)
+                {
+                    if(h == cal.get(Calendar.HOUR_OF_DAY) && m >= cal.get(Calendar.MINUTE))
+                    {
+                    	if(h == 23 && m <= 29 && m >= 0)
+                    		check = true;
+                    	else if(h != 23 && m >= 0 && m <= 59)
+                    		check = true;
+                    }
+                    else if(h != cal.get(Calendar.HOUR_OF_DAY))
+                    {
+                    	if(h != 23 && m >= 0 && m <= 59)
+                    		check = true;
+                    	else if(h == 23 && m <= 29 && m >= 0)
+                    		check = true;
+                    }
+                }
+                else if((d != cal.get(Calendar.DATE) || y != cal.get(Calendar.YEAR) || mon != (cal.get(Calendar.MONTH) + 1)))
+                {
+                	if(h >= 0 && h < 23 && m >= 0 && m <= 59)
+                		check = true;
+                	else if(h == 23 && m <= 29 && m >= 0)
+                		check = true;
+                }
+            }
+            return check;
+        }
+		public boolean validateToTime(String to, String date)
+		{
+			Calendar cal = Calendar.getInstance();
+            boolean check = false;    
+            if(to.matches("\\d{2}:\\d{2}"))
+            {
+                int h = Integer.parseInt(to.substring(0, 2));
+                int m = Integer.parseInt(to.substring(3, to.length()));
+                int y = Integer.parseInt(date.substring(0, 4));
+                int mon = Integer.parseInt(date.substring(5, 7));
+                int d = Integer.parseInt(date.substring(8, date.length()));
+                
+                if(d == cal.get(Calendar.DATE) && y == cal.get(Calendar.YEAR) && mon == (cal.get(Calendar.MONTH) + 1) && h >= cal.get(Calendar.HOUR_OF_DAY) && h <= 23)
+                {
+                    if(h == cal.get(Calendar.HOUR_OF_DAY) && m >= cal.get(Calendar.MINUTE))
+                    {
+                    	if(h == 0 && m <= 59 && m >= 29)
+                    		check = true;
+                    	else if(h != 0 && m >= 0 && m <= 59)
+                    		check = true;
+                    }
+                    else if(h != cal.get(Calendar.HOUR_OF_DAY))
+                    {
+                    	if(h != 0 && m >= 0 && m <= 59)
+                    		check = true;
+                    	else if(h == 0 && m <= 59 && m >= 29)
+                    		check = true;
+                    }
+                }
+                else if((d != cal.get(Calendar.DATE) || y != cal.get(Calendar.YEAR) || mon != (cal.get(Calendar.MONTH) + 1)))
+                {
+                	if(h > 0 && h <= 23 && m >= 0 && m <= 59)
+                		check = true;
+                	else if(h == 0 && m <= 59 && m >= 29)
+                		check = true;
+                }
+            }
+            return check;			
 		}
 }
